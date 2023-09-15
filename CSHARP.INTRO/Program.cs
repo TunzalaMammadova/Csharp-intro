26a31f3591a4dee488f7c4c38fbbb5f9a9618042
TunzalaMammadova <tunzalatma@code.edu.az>
2023-09-15 21:10:48 +0400

    added csharpintro file

---
+++ CSHARP.INTRO/Program.cs
@@ -0,0 +1,258 @@
+﻿
+//int studentAge = 25;
+
+//string name1 = "XXX";
+//string name2 = "YYY";
+
+//string result = name2 + name1;
+
+//Console.WriteLine(result);
+
+
+//bool isOnline = true;
+//string name = "sdasd";
+
+
+//Console.WriteLine(name.GetType());
+
+//bool isOnline = true;
+
+//if (isOnline)
+//{
+//    Console.WriteLine("Yes");
+//}
+//else
+//{
+//    Console.WriteLine("No");
+//}
+
+
+
+
+//int age = 29;
+
+//if (age > 30)
+//{
+//    Console.WriteLine("Qocalirsan");
+//}
+//else if (age == 29)
+//{
+//    Console.WriteLine("Yaxshisan");
+//}
+//else
+//{
+//    Console.WriteLine("Gencsen");
+//}
+
+
+
+
+//int number = 30;
+
+
+//string result = number > 30 ? "Qocasan" : "Yaxshisan";
+
+//Console.WriteLine(result);
+
+//bool res = number > 30 ? true : false;
+
+//Console.WriteLine(res);
+
+
+//if (number > 30)
+//{
+//    Console.WriteLine(true):
+//}
+//else
+//{
+//    Console.WriteLine(false);
+//}
+
+
+
+//int x = 10, y = 100;
+
+//string result = x > y ? "x is greater than y" :
+//                    x < y ? "x is less than y" :
+//                        x == y ? "x is equal to y" : "No result";
+
+//Console.WriteLine(result);
+
+//string name = null;
+
+//var result = name ?? "XXX";
+
+//Console.WriteLine(result);
+
+//Console.WriteLine("Add your email:");
+
+//string email = Console.ReadLine();
+
+
+//Console.WriteLine("add your password:");
+
+//string password = Console.ReadLine();
+
+//if (email == "cavid@gmail.com" && password == "123")
+//{
+//    Console.WriteLine("Login success");
+//}
+//else
+
+//{
+//    Console.WriteLine("Email or password is wrong");
+
+//}
+
+
+
+
+//int weekDay = 10;
+
+//switch (weekDay)
+//{
+//    case 1:
+//        Console.WriteLine("Monday")
+//        break;
+//    case 2:
+//        Console.WriteLine("Tuesday");
+//        break;
+//    case 3:
+//        Console.WriteLine("Wednesday");
+//        break;
+//    default:
+//        Console.WriteLine("Week day not found");
+//        break;
+//}
+
+
+//int[] nums1 = new int[10];
+
+//int[] nums2 = new int[3] { 1, 4, 5 };
+
+//int[] nums = { 3, 2, 6, 8, 9 };
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        Console.WriteLine(nums[i]);
+//        break;
+//    }
+//}
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+
+//int count = 0;
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+
+//        Console.WriteLine(i);
+//    }
+//}
+
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+
+//int count = 0;
+
+//int sum = 0;
+
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+
+//        sum += i;
+//    }
+//}
+
+//Console.WriteLine(sum);
+
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+
+//int count = 0;
+
+//int sumElem = 0;
+
+//int sumIndex = 0;
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+
+//        sumElem += nums[i];
+//        sumIndex += i;
+//    }
+//}
+
+//int result = sumElem - sumIndex;
+
+
+//Console.WriteLine(result);
+
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        Console.WriteLine(nums[i]);
+//        break;
+
+//    }
+//}
+
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        continue;
+
+//    }
+
+//    Console.WriteLine(nums[i]);
+
+//}
+
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+
+
+//foreach (var item in nums)
+//{
+//    if (item == 2)
+//    {
+//        Console.WriteLine(item);
+//        break;
+
+//    }
+//}
+
+
+//DateTime date = DateTime.Now;
+
+//Console.WriteLine(date);
+
+
+//string date = DateTime.Now.ToString("MM-dd-yyyy HH:mm");
+
+//Console.WriteLine(date);
+
+
